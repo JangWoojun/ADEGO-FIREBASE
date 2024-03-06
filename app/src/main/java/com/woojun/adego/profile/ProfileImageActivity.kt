c4f6a92bd840708a078801358c2f4f4220324f37
JangWoojun <woojun0107@naver.com>
2024-03-06 20:08:10 +0900

    feat: ProfileActivitys 추가

---
+++ app/src/main/java/com/woojun/adego/profile/ProfileImageActivity.kt
@@ -0,0 +1,12 @@
+package com.woojun.adego.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.woojun.adego.R
+
+class ProfileImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_image)
+    }
+}
---
+++ app/src/main/java/com/woojun/adego/profile/ProfileNameActivity.kt
@@ -0,0 +1,12 @@
+package com.woojun.adego.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.woojun.adego.R
+
+class ProfileNameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_name)
+    }
+}
