7ea19b1591007cdfcc1a4f61e8a7abb48e535780
JangWoojun <woojun0107@naver.com>
2024-03-08 04:22:44 +0900

    feat: 카카오 로그인 로직 구현 및 User 데이터 클래스 추가

--- app/src/main/java/com/woojun/adego/LoginActivity.kt
+++ app/src/main/java/com/woojun/adego/LoginActivity.kt
@@ -1,24 +1,87 @@
 package com.woojun.adego
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import com.woojun.adego.databinding.ActivityLoginBinding
 import com.woojun.adego.profile.ProfileNameActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        database = Firebase.database.reference
 
         binding.googleLoginButton.setOnClickListener {
             startActivity(Intent(this@LoginActivity, ProfileNameActivity::class.java))
         }
 
         binding.kakaoLoginButton.setOnClickListener {
+            kakaoLogin(this@LoginActivity)
+        }
+    }
+
+    private fun kakaoLogin(context: Context) {
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                Toast.makeText(context, "카카오 로그인에 실패하였습니다", Toast.LENGTH_SHORT).show()
+            } else if (token != null) {
+                kakaoGetUserProfile(context)
+            }
+        }
+
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+            UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+                if (error != null) {
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+
+                    UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+                } else if (token != null) {
+                    kakaoGetUserProfile(context)
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+        }
+    }
+
+    private fun kakaoGetUserProfile(context: Context) {
+        UserApiClient.instance.me { user, error ->
+            if (error != null) {
+                Toast.makeText(context, "카카오 로그인에 실패하였습니다", Toast.LENGTH_SHORT).show()
+            }
+            else if (user?.id != null) {
+                kakaoSignUp(user.id.toString(), user.kakaoAccount!!.profile!!.nickname!!, user.kakaoAccount!!.profile!!.thumbnailImageUrl!!)
+            }
+        }
+    }
+
+    private fun kakaoSignUp(userId: String, userNickname: String, userProfileImage: String) {
+        AppPreferences.id = userId
+        AppPreferences.nickname = userNickname
+        AppPreferences.profileImage = userProfileImage
+
+        database.child("users").child(userId).get().addOnSuccessListener {
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+        }.addOnFailureListener{
             startActivity(Intent(this@LoginActivity, ProfileNameActivity::class.java))
         }
     }
+
 }
---
+++ app/src/main/java/com/woojun/adego/User.kt
@@ -0,0 +1,6 @@
+package com.woojun.adego
+
+data class User(
+    var nickname: String,
+    var profileImage: String
+)
