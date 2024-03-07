2d0daa7eff7819f0712fa19b2fe244ada62b3292
JangWoojun <woojun0107@naver.com>
2024-03-08 05:13:46 +0900

    fix: 이미지 업로드 전 화면 전환 방지

--- app/src/main/java/com/woojun/adego/profile/ProfileImageActivity.kt
+++ app/src/main/java/com/woojun/adego/profile/ProfileImageActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.Paint
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +19,11 @@ import com.woojun.adego.MainActivity
 import com.woojun.adego.R
 import com.woojun.adego.User
 import com.woojun.adego.databinding.ActivityProfileImageBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class ProfileImageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileImageBinding
@@ -28,12 +34,26 @@ class ProfileImageActivity : AppCompatActivity() {
     private val getContent: ActivityResultLauncher<String> =
         registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
             uri?.let {
-                image = imageUpload(it)
-                Glide.with(this@ProfileImageActivity)
-                    .load(it)
-                    .centerCrop()
-                    .diskCacheStrategy(DiskCacheStrategy.ALL)
-                    .into(binding.imageView)
+                CoroutineScope(Dispatchers.IO).launch {
+                    withContext(Dispatchers.Main) {
+                        binding.noneButton.visibility = View.VISIBLE
+                        binding.nextButton.visibility = View.GONE
+
+                        if (!isDestroyed) {
+                            Glide.with(this@ProfileImageActivity)
+                                .load(it)
+                                .centerCrop()
+                                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                                .into(binding.imageView)
+                        }
+                    }
+                    image = imageUpload(it)
+
+                    withContext(Dispatchers.Main) {
+                        binding.noneButton.visibility = View.GONE
+                        binding.nextButton.visibility = View.VISIBLE
+                    }
+                }
             }
         }
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,24 +92,25 @@ class ProfileImageActivity : AppCompatActivity() {
         overridePendingTransition(R.anim.anim_slide_in_from_left_fade_in, R.anim.anim_fade_out)
     }
 
-    private fun imageUpload(fileUri: Uri): String {
-        val storage = Firebase.storage
-        val storageRef = storage.reference
+    private suspend fun imageUpload(fileUri: Uri): String {
+        try {
+            val storage = Firebase.storage
+            val storageRef = storage.reference
 
-        var imageUrl = "https://firebasestorage.googleapis.com/v0/b/adego-6a7be.appspot.com/o/adego.png?alt=media&token=eb0056f3-35a9-4c7c-8304-2d93eed06803"
+            val imageRef = storageRef.child("images/${AppPreferences.id}")
+            val uploadTask = imageRef.putFile(fileUri)
 
-        val imageRef = storageRef.child("images/${AppPreferences.id}")
-        val uploadTask = fileUri.let { uri ->
-            imageRef.putFile(uri)
+            val taskSnapshot = uploadTask.await()
+            val imageUrl = taskSnapshot.metadata?.reference?.downloadUrl?.await().toString()
+
+            AppPreferences.profileImage = imageUrl
+
+            return imageUrl
+        } catch (e: Exception) {
+            val defaultImageUrl = "https://firebasestorage.googleapis.com/v0/b/adego-6a7be.appspot.com/o/adego.png?alt=media&token=eb0056f3-35a9-4c7c-8304-2d93eed06803"
+            AppPreferences.profileImage = defaultImageUrl
+            return defaultImageUrl
         }
-
-        uploadTask.addOnSuccessListener { taskSnapshot ->
-            taskSnapshot.metadata?.reference?.downloadUrl?.addOnSuccessListener { uri ->
-                imageUrl = uri.toString()
-            }
-        }
-        AppPreferences.profileImage = imageUrl
-
-        return imageUrl
     }
+
 }
