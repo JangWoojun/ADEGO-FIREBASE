b129351ce8c23b4707dfa6d7abbc98d8bf7d05b5
JangWoojun <woojun0107@naver.com>
2024-03-07 16:30:24 +0900

    feat: 로그인 버튼 임시 화면 전환

--- app/src/main/java/com/woojun/adego/LoginActivity.kt
+++ app/src/main/java/com/woojun/adego/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.woojun.adego
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.woojun.adego.databinding.ActivityLoginBinding
+import com.woojun.adego.profile.ProfileNameActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.googleLoginButton.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, ProfileNameActivity::class.java))
+        }
+
+        binding.kakaoLoginButton.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, ProfileNameActivity::class.java))
+        }
     }
 }
