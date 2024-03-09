504f62834ff90b007f85680059e5dd3e1ae4b4de
JangWoojun <woojun0107@naver.com>
2024-03-10 04:43:42 +0900

    feat: InviteActivity 추가

---
+++ app/src/main/java/com/woojun/adego/promise/InviteActivity.kt
@@ -0,0 +1,12 @@
+package com.woojun.adego.promise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.woojun.adego.R
+
+class InviteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_invite)
+    }
+}
