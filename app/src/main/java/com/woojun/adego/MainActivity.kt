4cec92684b0196a8fcbfc7452c539fd7c0839792
JangWoojun <woojun0107@naver.com>
2024-03-09 14:57:05 +0900

    feat: google map api 구현

--- app/src/main/java/com/woojun/adego/MainActivity.kt
+++ app/src/main/java/com/woojun/adego/MainActivity.kt
@@ -2,10 +2,36 @@ package com.woojun.adego
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.woojun.adego.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val marker = LatLng(37.62759321424639, 126.92311223239273)
+        mMap.addMarker(
+            MarkerOptions()
+            .position(marker)
+            .title("세명컴퓨터고등학교"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, 18.0F))
     }
 }
