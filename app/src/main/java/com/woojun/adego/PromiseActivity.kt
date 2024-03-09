7f7d82d609c855e454ef6caac2669413b899c55c
JangWoojun <woojun0107@naver.com>
2024-03-10 01:27:07 +0900

    feat: 약속 관련 액티비티 추가

---
+++ app/src/main/java/com/woojun/adego/PromiseActivity.kt
@@ -0,0 +1,11 @@
+package com.woojun.adego
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PromiseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_promise)
+    }
+}
