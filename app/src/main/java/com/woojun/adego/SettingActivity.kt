eeb0389f2efdb7cf77f4c804231f3b6d84471923
JangWoojun <woojun0107@naver.com>
2024-03-11 10:33:33 +0900

    feat: SettingActivity 버튼 클릭 시 효과 구현

--- app/src/main/java/com/woojun/adego/SettingActivity.kt
+++ app/src/main/java/com/woojun/adego/SettingActivity.kt
@@ -1,8 +1,16 @@
 package com.woojun.adego
 
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.woojun.adego.databinding.ActivitySettingBinding
+import com.woojun.adego.profile.ProfileNameActivity
 
 class SettingActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySettingBinding
@@ -10,5 +18,46 @@ class SettingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.logoutButton.setOnClickListener {
+            val customDialog = Dialog(this@SettingActivity)
+            customDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            customDialog.window?.requestFeature(Window.FEATURE_NO_TITLE)
+
+            customDialog.setContentView(R.layout.dialog_setting)
+
+            customDialog.findViewById<TextView>(R.id.main_button_text).text = "로그아웃"
+            customDialog.findViewById<CardView>(R.id.main_button).setOnClickListener {
+                finish()
+            }
+            customDialog.findViewById<CardView>(R.id.cancel_button).setOnClickListener {
+                customDialog.cancel()
+            }
+
+            customDialog.show()
+        }
+
+        binding.withdrawalButton.setOnClickListener {
+            val customDialog = Dialog(this@SettingActivity)
+            customDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            customDialog.window?.requestFeature(Window.FEATURE_NO_TITLE)
+
+            customDialog.setContentView(R.layout.dialog_setting)
+
+            customDialog.findViewById<TextView>(R.id.main_button_text).text = "탈퇴"
+            customDialog.findViewById<CardView>(R.id.main_button).setOnClickListener {
+                finish()
+            }
+            customDialog.findViewById<CardView>(R.id.cancel_button).setOnClickListener {
+                customDialog.cancel()
+            }
+
+            customDialog.show()
+        }
+
+        binding.changeImageButton.setOnClickListener {
+            startActivity(Intent(this@SettingActivity, ProfileNameActivity::class.java))
+        }
+
     }
 }
