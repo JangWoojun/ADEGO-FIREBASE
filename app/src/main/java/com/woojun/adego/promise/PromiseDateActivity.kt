413100efd3dce2a60a575169aa545f082d358bc6
JangWoojun <woojun0107@naver.com>
2024-03-13 11:07:43 +0900

    feat: calendarView 헤더 날짜 형식 변경 및 날짜 클릭 시 액티비티 이동 구현

--- app/src/main/java/com/woojun/adego/promise/PromiseDateActivity.kt
+++ app/src/main/java/com/woojun/adego/promise/PromiseDateActivity.kt
@@ -1,16 +1,12 @@
 package com.woojun.adego.promise
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
-import com.prolificinteractive.materialcalendarview.MaterialCalendarView
-import com.prolificinteractive.materialcalendarview.format.TitleFormatter
-import com.woojun.adego.R
+import androidx.appcompat.app.AppCompatActivity
 import com.woojun.adego.databinding.ActivityPromiseDateBinding
-import java.text.SimpleDateFormat
-import java.time.format.DateTimeFormatter
-import java.util.Locale
+import java.time.Instant
+
 
 class PromiseDateActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPromiseDateBinding
@@ -19,10 +15,24 @@ class PromiseDateActivity : AppCompatActivity() {
         binding = ActivityPromiseDateBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.calendarView.addDecorators(PastDateDecorator(this), FutureDateDecorator(this), )
+        binding.calendarView.addDecorators(PastDateDecorator(this), FutureDateDecorator(this))
 
-        binding.calendarView.setOnDateChangedListener { widget, date, selected ->
-            Toast.makeText(this@PromiseDateActivity, date.toString(), Toast.LENGTH_SHORT).show()
+        binding.calendarView.setOnDateChangedListener { _, date, _ ->
+            startActivity(Intent(this@PromiseDateActivity, PromiseTimeActivity::class.java).apply {
+                this.putExtra("date", date.date.toString())
+            })
+        }
+
+        binding.calendarView.setTitleFormatter { day ->
+            val inputText = day.date
+
+            val calendarHeaderElements = inputText.toString().split("-")
+            val calendarHeaderBuilder = StringBuilder()
+
+            calendarHeaderBuilder.append(calendarHeaderElements[0]).append("년 ")
+                .append(calendarHeaderElements[1]).append("월")
+
+            calendarHeaderBuilder.toString()
         }
     }
 }
