797ffc06ab8a9c7f68f65d113e1a823890365578
JangWoojun <woojun0107@naver.com>
2024-03-15 11:01:31 +0900

    feat: 카카오 키워드로 장소 검색하기 response data class 들 추가

---
+++ app/src/main/java/com/woojun/adego/dataClass/KakaoLocation.kt
@@ -0,0 +1,6 @@
+package com.woojun.adego.dataClass
+
+data class KakaoLocation(
+    val documents: List<Document>,
+    val meta: Meta
+)
---
+++ app/src/main/java/com/woojun/adego/dataClass/SameName.kt
@@ -0,0 +1,7 @@
+package com.woojun.adego.dataClass
+
+data class SameName(
+    val keyword: String,
+    val region: List<Any>,
+    val selected_region: String
+)
