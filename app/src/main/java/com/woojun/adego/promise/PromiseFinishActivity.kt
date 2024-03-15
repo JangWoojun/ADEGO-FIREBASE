2552d77c4b6d8e815fd4f7dfe7bf68c344177675
JangWoojun <woojun0107@naver.com>
2024-03-16 04:45:38 +0900

    feat: AppPreferences에서 값 가져와서 binding

--- app/src/main/java/com/woojun/adego/promise/PromiseFinishActivity.kt
+++ app/src/main/java/com/woojun/adego/promise/PromiseFinishActivity.kt
@@ -1,7 +1,12 @@
 package com.woojun.adego.promise
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.woojun.adego.AppPreferences
+import com.woojun.adego.MainActivity
+import com.woojun.adego.OnSingleClickListener
 import com.woojun.adego.R
 import com.woojun.adego.databinding.ActivityPromiseFinishBinding
 
@@ -15,8 +20,33 @@ class PromiseFinishActivity : AppCompatActivity() {
 
         binding.backButton.setOnClickListener {
             overridePendingTransition(R.anim.anim_slide_in_from_left_fade_in, R.anim.anim_fade_out)
+            startActivity(Intent(this@PromiseFinishActivity, MainActivity::class.java))
             finish()
         }
+
+        binding.nameText.text = AppPreferences.promiseName
+        binding.dateText.text = AppPreferences.promiseDate
+        binding.timeText.text = AppPreferences.promiseTime
+        binding.locationText.text = AppPreferences.promiseLocation
+
+        binding.finishButton.setOnClickListener {
+            overridePendingTransition(R.anim.anim_slide_in_from_left_fade_in, R.anim.anim_fade_out)
+            startActivity(Intent(this@PromiseFinishActivity, MainActivity::class.java))
+            finish()
+        }
+
+        binding.sharedButton.setOnClickListener(object : OnSingleClickListener() {
+            override fun onSingleClick(v: View?) {
+                startActivity(
+                    Intent(Intent.ACTION_SEND_MULTIPLE).apply {
+                        type = "text/plain"
+                        val url = ""
+                        val content = "약속에 초대됐어요!\n하단 링크를 통해 어떤 약속인지 확인하세요."
+                        putExtra(Intent.EXTRA_TEXT,"$content\n\n$url")
+                    }
+                )
+            }
+        })
     }
 
     override fun onBackPressed() {
