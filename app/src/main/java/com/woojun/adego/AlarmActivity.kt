53b4f09b9468ec9b05784b3fbb9a47b936a7bc96
JangWoojun <woojun0107@naver.com>
2024-03-18 13:29:35 +0900

    feat: AlarmAdapter 연결 및 세팅

--- app/src/main/java/com/woojun/adego/AlarmActivity.kt
+++ app/src/main/java/com/woojun/adego/AlarmActivity.kt
@@ -1,11 +1,14 @@
 package com.woojun.adego
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.woojun.adego.databinding.ActivityAlarmBinding
 
 class AlarmActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAlarmBinding
+    private val list = mutableListOf<User>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAlarmBinding.inflate(layoutInflater)
@@ -14,5 +17,13 @@ class AlarmActivity : AppCompatActivity() {
         binding.backButton.setOnClickListener {
             finish()
         }
+
+        binding.friendsList.layoutManager = GridLayoutManager(this@AlarmActivity, 2)
+        binding.friendsList.adapter = AlarmAdapter(list)
+
+        binding.friendsList.addItemDecoration(EdgeItemDecoration(2, 8f.fromDpToPx()))
     }
+
+    private fun Float.fromDpToPx(): Int =
+        (this * Resources.getSystem().displayMetrics.density).toInt()
 }
